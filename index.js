6d3e949d967a5bfc86f6be7a4cf7696aa728d0aa
Bundy-Mundi <bennie.ben095@gmail.com>
2021-04-08 01:24:43 +0900

    Add body-parser

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 const app = express();
 const blockchian = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get("/api/blocks", (req, res) => {
     res.json(blockchian.chain);
 });
 
-app.listen(PORT, () => console.log(`Listening to: http://localhost:${PORT}`))
+app.post("/api/mine", (req, res) => {
+    const { data } = req.body;
+    blockchian.addBlock({ data });
+
+    res.redirect("/api/blocks");
+});
+
+
+
+app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`))
